501b1b8daf37702bd8d143ecfaf89615c251c302
jfcerezo@comillas.edu <jfcerezo@comillas.edu>
2023-02-04 13:20:09 +0100

    modified:   execute-test.sh

--- execute-test.sh
+++ execute-test.sh
@@ -8,10 +8,10 @@ echo "Start time : " $(TZ=CET date) "-----------------------"
 for COUNT in $(seq 1 100)
 do
    TSTART=$(date +%s%N | cut -b1-13)
-   TZ=CET date "+%Y-%m-%d %H:%M:%S"
+   TZ=CET date "+test_$COUNT starts %Y-%m-%d %H:%M:%S"
    /apache-zookeeper-3.7.0-bin/bin/zkCli.sh < data.cmd > /dev/null
    TEND=$(date +%s%N | cut -b1-13)
-   echo test $COUNT tiempo_ms $((TEND - TSTART))
+   echo test_$COUNT tiempo_ms $((TEND - TSTART))
 done
 
 echo "end time : " $(TZ=CET date) "-----------------------"
